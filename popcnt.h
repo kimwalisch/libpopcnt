482349664b07baefb03a397af66dbb083531631d
Kim Walisch <kim.walisch@gmail.com>
2016-11-28 20:12:19 +0100

    Harley-Seal popcount (4th iteration)

--- popcnt.h
+++ popcnt.h
@@ -63,7 +63,7 @@ inline uint64_t popcnt_u64(uint64_t x)
   const uint64_t h01 = 0x0101010101010101ll;
 
   x -= (x >> 1) & m1;
-  x = (x & m2) + ((x >> 2)  & m2);
+  x = (x & m2) + ((x >> 2) & m2);
   x = (x + (x >> 4)) & m4;
 
   return (x * h01) >> 56;
@@ -76,40 +76,49 @@ inline void CSA(uint64_t& h, uint64_t& l, uint64_t a, uint64_t b, uint64_t c)
   l = u ^ c;
 }
 
-/// Harley-Seal popcount (3rd iteration).
+/// Harley-Seal popcount (4th iteration).
 /// The Harley-Seal popcount algorithm is one of the fastest algorithms
 /// for counting 1 bits in an array using only integer operations.
-/// This implementation uses only 6.38 instructions per 64-bit word.
+/// This implementation uses only 5.69 instructions per 64-bit word.
 /// @see Chapter 5 in "Hacker's Delight" 2nd edition.
 ///
-inline uint64_t popcnt_array_u64(const uint64_t* array, uint64_t size)
+uint64_t popcnt_array_u64(const uint64_t* data, const uint64_t size)
 {
   uint64_t total = 0;
-  uint64_t ones = 0, twos = 0, fours = 0, eights = 0;
-  uint64_t twosA, twosB, foursA, foursB;
-  uint64_t limit = size - size % 8;
+  uint64_t ones = 0, twos = 0, fours = 0, eights = 0, sixteens = 0;
+  uint64_t twosA, twosB, foursA, foursB, eightsA, eightsB;
+  uint64_t limit = size - size % 16;
   uint64_t i = 0;
 
-  for(; i < limit; i += 8)
+  for(; i < limit; i += 16)
   {
-    CSA(twosA, ones, ones, array[i+0], array[i+1]);
-    CSA(twosB, ones, ones, array[i+2], array[i+3]);
-    CSA(foursA, twos, twos, twosA, twosB);    
-    CSA(twosA, ones, ones, array[i+4], array[i+5]);
-    CSA(twosB, ones, ones, array[i+6], array[i+7]);
+    CSA(twosA, ones, ones, data[i+0], data[i+1]);
+    CSA(twosB, ones, ones, data[i+2], data[i+3]);
+    CSA(foursA, twos, twos, twosA, twosB);
+    CSA(twosA, ones, ones, data[i+4], data[i+5]);
+    CSA(twosB, ones, ones, data[i+6], data[i+7]);
     CSA(foursB, twos, twos, twosA, twosB);
-    CSA(eights, fours, fours, foursA, foursB);
+    CSA(eightsA,fours, fours, foursA, foursB);
+    CSA(twosA, ones, ones, data[i+8], data[i+9]);
+    CSA(twosB, ones, ones, data[i+10], data[i+11]);
+    CSA(foursA, twos, twos, twosA, twosB);
+    CSA(twosA, ones, ones, data[i+12], data[i+13]);
+    CSA(twosB, ones, ones, data[i+14], data[i+15]);
+    CSA(foursB, twos, twos, twosA, twosB);
+    CSA(eightsB, fours, fours, foursA, foursB);
+    CSA(sixteens, eights, eights, eightsA, eightsB);
 
-    total += popcnt_u64(eights);
+    total += popcnt_u64(sixteens);
   }
 
-  total *= 8;
+  total *= 16;
+  total += 8 * popcnt_u64(eights);
   total += 4 * popcnt_u64(fours);
   total += 2 * popcnt_u64(twos);
   total += 1 * popcnt_u64(ones);
 
   for(; i < size; i++)
-    total += popcnt_u64(array[i]);
+    total += popcnt_u64(data[i]);
 
   return total;
 }
