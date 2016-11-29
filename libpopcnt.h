85ad6fa8f6a422ebb3638507546c99f0f1de4143
Kim Walisch <kim.walisch@gmail.com>
2016-11-29 17:56:59 +0100

    Rename popcnt.h to libpopcnt.h

--- libpopcnt.h
+++ libpopcnt.h
@@ -1,5 +1,33 @@
-#ifndef POPCNT_H
-#define POPCNT_H
+// libpopcnt.h - C/C++ library for counting the number of 1 bits (bit
+// population count) in an array as quickly as possible using
+// specialized CPU instructions e.g. POPCNT, AVX2.
+//
+// Copyright (c) 2016, Kim Walisch
+// Copyright (c) 2016, Wojciech Muła
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef LIBPOPCNT_H
+#define LIBPOPCNT_H
 
 #include <stdint.h>
 
@@ -264,4 +292,4 @@ inline uint64_t popcnt(const uint8_t* data, uint64_t size)
   return total;
 }
 
-#endif /* POPCNT_H */
+#endif /* LIBPOPCNT_H */
