783b2a1e46ecb98b74f7d37f5bc3e6a1c5f9eb1f
Kim Walisch <kim.walisch@gmail.com>
2017-03-28 21:37:41 +0200

    Use anonymous namespace

--- libpopcnt.h
+++ libpopcnt.h
@@ -92,12 +92,27 @@
   #define HAS_AVX2
 #endif
 
+#if defined(HAS_AVX2)
+  #include <immintrin.h>
+#endif
+
+// MSVC, x86
+#if defined(_MSC_VER) && \
+   (defined(_M_X64) || \
+    defined(_M_IX86))
+  #include <intrin.h>
+  #include <immintrin.h>
+  #include <nmmintrin.h>
+#endif
+
+namespace {
+
 /// This uses fewer arithmetic operations than any other known
 /// implementation on machines with fast multiplication.
 /// It uses 12 arithmetic operations, one of which is a multiply.
 /// http://en.wikipedia.org/wiki/Hamming_weight#Efficient_implementation
 ///
-static inline uint64_t popcount64(uint64_t x)
+inline uint64_t popcount64(uint64_t x)
 {
   uint64_t m1 = 0x5555555555555555ll;
   uint64_t m2 = 0x3333333333333333ll;
@@ -114,7 +129,7 @@ static inline uint64_t popcount64(uint64_t x)
 #if defined(HAS_ASM_POPCNT) && \
     defined(__x86_64__)
 
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   __asm__ ("popcnt %1, %0" : "=r" (x) : "0" (x));
   return x;
@@ -123,13 +138,13 @@ static inline uint64_t popcnt64(uint64_t x)
 #elif defined(HAS_ASM_POPCNT) && \
       defined(__i386__)
 
-static inline uint32_t popcnt32(uint32_t x)
+inline uint32_t popcnt32(uint32_t x)
 {
   __asm__ ("popcnt %1, %0" : "=r" (x) : "0" (x));
   return x;
 }
 
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   return popcnt32((uint32_t) x) +
          popcnt32((uint32_t)(x >> 32));
@@ -138,9 +153,7 @@ static inline uint64_t popcnt64(uint64_t x)
 #elif defined(_MSC_VER) && \
       defined(_M_X64)
 
-#include <nmmintrin.h>
-
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   return _mm_popcnt_u64(x);
 }
@@ -148,9 +161,7 @@ static inline uint64_t popcnt64(uint64_t x)
 #elif defined(_MSC_VER) && \
       defined(_M_IX86)
 
-#include <nmmintrin.h>
-
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   return _mm_popcnt_u32((uint32_t) x) + 
          _mm_popcnt_u32((uint32_t)(x >> 32));
@@ -159,7 +170,7 @@ static inline uint64_t popcnt64(uint64_t x)
 // non x86 CPUs
 #elif defined(HAS_BUILTIN_POPCOUNT)
 
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   return __builtin_popcountll(x);
 }
@@ -168,106 +179,14 @@ static inline uint64_t popcnt64(uint64_t x)
 // use integer popcnt64 implementation
 #else
 
-static inline uint64_t popcnt64(uint64_t x)
+inline uint64_t popcnt64(uint64_t x)
 {
   return popcount64(x);
 }
 
 #endif
 
-// x86 cpuid
-#if defined(__x86_64__) || \
-    defined(__i386__) || \
-    defined(_M_X64) || \
-    defined(_M_IX86)
-
-#if defined(_MSC_VER)
-  #include <intrin.h>
-#endif
-
-// %ecx bit flags
-#define bit_POPCNT (1 << 23)
-
-// %ebx bit flags
-#define bit_AVX2 (1 << 5)
-
-static inline void run_cpuid(int eax, int ecx, int* abcd)
-{
-  int ebx = 0;
-  int edx = 0;
-
-#if defined(_MSC_VER)
-  __cpuidex(abcd, eax, ecx);
-
-#elif defined(__i386__) && \
-      defined(__PIC__)
-  // in case of PIC under 32-bit EBX cannot be clobbered
-  __asm__ ("movl %%ebx, %%edi;"
-           "cpuid;"
-           "xchgl %%ebx, %%edi;"
-           : "=D" (ebx),
-             "+a" (eax),
-             "+c" (ecx),
-             "=d" (edx));
-#else
-  __asm__ ("cpuid;"
-           : "+b" (ebx),
-             "+a" (eax),
-             "+c" (ecx),
-             "=d" (edx));
-#endif
-  abcd[0] = eax;
-  abcd[1] = ebx;
-  abcd[2] = ecx;
-  abcd[3] = edx;
-}
-
-#if defined(HAS_AVX2)
-
-static inline int check_xcr0_ymm()
-{
-  int xcr0;
-  __asm__ ("xgetbv" : "=a" (xcr0) : "c" (0) : "%edx" );
-  return ((xcr0 & 6) == 6);
-}
-
-static inline int has_AVX2()
-{
-  int abcd[4];
-  int osxsave_mask = (1 << 27);
-
-  // must ensure OS supports extended processor state management
-  run_cpuid(1, 0, abcd);
-  if ((abcd[2] & osxsave_mask) != osxsave_mask)
-    return 0;
-
-  // must ensure OS supports ZMM registers (and YMM, and XMM)
-  if (!check_xcr0_ymm())
-    return 0;
-
-  run_cpuid(7, 0, abcd);
-  if ((abcd[1] & bit_AVX2) != bit_AVX2)
-    return 0;
-
-  return bit_AVX2;
-}
-
-#endif /* HAS_AVX2 */
-
-static inline int has_POPCNT()
-{
-  int abcd[4];
-
-  run_cpuid(1, 0, abcd);
-  if ((abcd[2] & bit_POPCNT) != bit_POPCNT)
-    return 0;
-
-  return bit_POPCNT;
-}
-
-#endif /* cpuid */
-
-static inline uint64_t popcnt64_unrolled(const uint64_t* data, uint64_t size)
+inline uint64_t popcnt64_unrolled(const uint64_t* data, uint64_t size)
 {
   uint64_t i = 0;
   uint64_t limit = size - size % 4;
@@ -290,7 +209,7 @@ static inline uint64_t popcnt64_unrolled(const uint64_t* data, uint64_t size)
 /// Carry-save adder (CSA).
 /// @see Chapter 5 in "Hacker's Delight".
 ///
-static inline void CSA(uint64_t& h, uint64_t& l, uint64_t a, uint64_t b, uint64_t c)
+inline void CSA(uint64_t& h, uint64_t& l, uint64_t a, uint64_t b, uint64_t c)
 {
   uint64_t u = a ^ b; 
   h = (a & b) | (u & c);
@@ -303,7 +222,7 @@ static inline void CSA(uint64_t& h, uint64_t& l, uint64_t a, uint64_t b, uint64_
 /// This implementation uses only 6.38 instructions per 64-bit word.
 /// @see Chapter 5 in "Hacker's Delight" 2nd edition.
 ///
-static inline uint64_t popcnt64_hs(const uint64_t* data, uint64_t size)
+inline uint64_t popcnt64_hs(const uint64_t* data, uint64_t size)
 {
   uint64_t cnt = 0;
   uint64_t ones = 0, twos = 0, fours = 0, eights = 0;
@@ -335,96 +254,97 @@ static inline uint64_t popcnt64_hs(const uint64_t* data, uint64_t size)
   return cnt;
 }
 
-// non x86 CPUs
-#if !defined(__x86_64__) && \
-    !defined(__i386__) && \
-    !defined(_M_X64) && \
-    !defined(_M_IX86)
+// x86 cpuid
+#if defined(__x86_64__) || \
+    defined(__i386__) || \
+    defined(_M_X64) || \
+    defined(_M_IX86)
 
-/// Align memory to 8 bytes boundary
-static inline void align8(const uint8_t*& p, uint64_t& size, uint64_t& cnt)
+// %ecx bit flags
+#define bit_POPCNT (1 << 23)
+
+// %ebx bit flags
+#define bit_AVX2 (1 << 5)
+
+inline void run_cpuid(int eax, int ecx, int* abcd)
 {
-  for (; size > 0 && (uintptr_t) p % 8; p++)
-  {
-    cnt += popcnt64(*p);
-    size -= 1;
-  }
-}
+  int ebx = 0;
+  int edx = 0;
 
-/// Count the number of 1 bits in the data array.
-/// @param data  An array
-/// @param size  Size of data in bytes
-///
-static uint64_t popcnt(const void* data, uint64_t size)
-{
-  uint64_t cnt = 0;
-
-  const uint8_t* data8 = (const uint8_t*) data;
-  align8(data8, size, cnt);
-
-  cnt += popcnt64_unrolled((const uint64_t*) data8, size / 8);
-  data8 += size - size % 8;
-  size = size % 8;
-
-  for (uint64_t i = 0; i < size; i++)
-    cnt += popcnt64(data8[i]);
-
-  return cnt;
-}
-
+#if defined(_MSC_VER)
+  __cpuidex(abcd, eax, ecx);
+#elif defined(__i386__) && \
+      defined(__PIC__)
+  // in case of PIC under 32-bit EBX cannot be clobbered
+  __asm__ ("movl %%ebx, %%edi;"
+           "cpuid;"
+           "xchgl %%ebx, %%edi;"
+           : "=D" (ebx),
+             "+a" (eax),
+             "+c" (ecx),
+             "=d" (edx));
+#else
+  __asm__ ("cpuid;"
+           : "+b" (ebx),
+             "+a" (eax),
+             "+c" (ecx),
+             "=d" (edx));
 #endif
-
-// x86 CPUs, no avx2
-#if !defined(HAS_AVX2) && \
-    (defined(__x86_64__) || \
-     defined(__i386__) || \
-     defined(_M_X64) || \
-     defined(_M_IX86))
-
-/// Count the number of 1 bits in the data array.
-/// @param data  An array
-/// @param size  Size of data in bytes
-///
-static uint64_t popcnt(const void* data, uint64_t size)
-{
-  static const int cpuid = has_POPCNT();
-
-  uint64_t cnt = 0;
-  const uint8_t* data8 = (const uint8_t*) data;
-
-  if (cpuid & bit_POPCNT)
-  {
-    const uint64_t* data64 = (const uint64_t*) data8;
-    uint64_t size64 = size / 8;
-    cnt += popcnt64_unrolled(data64, size64);
-    data8 += size - size % 8;
-    size = size % 8;
-    for (uint64_t i = 0; i < size; i++)
-      cnt += popcnt64(data8[i]);
-
-    return cnt;
-  }
-
-  // pure integer algorithm
-  const uint64_t* data64 = (const uint64_t*) data8;
-  uint64_t size64 = size / 8;
-  cnt += popcnt64_hs(data64, size64);
-  data8 += size - size % 8;
-  size = size % 8;
-  for (uint64_t i = 0; i < size; i++)
-    cnt += popcount64(data8[i]);
-
-  return cnt;
+  abcd[0] = eax;
+  abcd[1] = ebx;
+  abcd[2] = ecx;
+  abcd[3] = edx;
 }
 
+inline int has_POPCNT()
+{
+  int abcd[4];
+
+  run_cpuid(1, 0, abcd);
+  if ((abcd[2] & bit_POPCNT) != bit_POPCNT)
+    return 0;
+
+  return bit_POPCNT;
+}
+
+inline int check_xcr0_ymm()
+{
+  int xcr0;
+#if defined(_MSC_VER)
+  xcr0 = (uint32_t) _xgetbv(0);
+#else
+  __asm__ ("xgetbv" : "=a" (xcr0) : "c" (0) : "%edx" );
 #endif
+  return (xcr0 & 6) == 6;
+}
+
+inline int has_AVX2()
+{
+  int abcd[4];
+  int osxsave_mask = (1 << 27);
+
+  // must ensure OS supports extended processor state management
+  run_cpuid(1, 0, abcd);
+  if ((abcd[2] & osxsave_mask) != osxsave_mask)
+    return 0;
+
+  // must ensure OS supports ZMM registers (and YMM, and XMM)
+  if (!check_xcr0_ymm())
+    return 0;
+
+  run_cpuid(7, 0, abcd);
+  if ((abcd[1] & bit_AVX2) != bit_AVX2)
+    return 0;
+
+  return bit_AVX2;
+}
+
+#endif /* cpuid */
 
 #if defined(HAS_AVX2)
 
-#include <immintrin.h>
-
 __attribute__ ((target ("avx2")))
-static inline void CSA256(__m256i& h, __m256i& l, __m256i a, __m256i b, __m256i c)
+inline void CSA256(__m256i& h, __m256i& l, __m256i a, __m256i b, __m256i c)
 {
   __m256i u = a ^ b;
   h = (a & b) | (u & c);
@@ -432,7 +352,7 @@ static inline void CSA256(__m256i& h, __m256i& l, __m256i a, __m256i b, __m256i 
 }
 
 __attribute__ ((target ("avx2")))
-static inline __m256i popcnt256(__m256i v)
+inline __m256i popcnt256(__m256i v)
 {
   __m256i lookup1 = _mm256_setr_epi8(
       4, 5, 5, 6, 5, 6, 6, 7,
@@ -464,7 +384,7 @@ static inline __m256i popcnt256(__m256i v)
 /// @see https://arxiv.org/abs/1611.07612
 ///
 __attribute__ ((target ("avx2")))
-static inline uint64_t popcnt_avx2(const __m256i* data, uint64_t size)
+inline uint64_t popcnt_avx2(const __m256i* data, uint64_t size)
 {
   __m256i cnt = _mm256_setzero_si256();
   __m256i ones = _mm256_setzero_si256();
@@ -516,7 +436,7 @@ static inline uint64_t popcnt_avx2(const __m256i* data, uint64_t size)
 }
 
 /// Align memory to 32 bytes boundary
-static inline void align_avx2(const uint8_t*& p, uint64_t& size, uint64_t& cnt)
+inline void align_avx2(const uint8_t*& p, uint64_t& size, uint64_t& cnt)
 {
   for (; (uintptr_t) p % 8; p++)
   {
@@ -531,17 +451,66 @@ static inline void align_avx2(const uint8_t*& p, uint64_t& size, uint64_t& cnt)
   }
 }
 
+#endif /* avx2 */
+
+// non x86 CPUs
+#if !defined(__x86_64__) && \
+    !defined(__i386__) && \
+    !defined(_M_X64) && \
+    !defined(_M_IX86)
+
+/// Align memory to 8 bytes boundary
+inline void align8(const uint8_t*& p, uint64_t& size, uint64_t& cnt)
+{
+  for (; size > 0 && (uintptr_t) p % 8; p++)
+  {
+    cnt += popcnt64(*p);
+    size -= 1;
+  }
+}
+
 /// Count the number of 1 bits in the data array.
 /// @param data  An array
 /// @param size  Size of data in bytes
 ///
-static uint64_t popcnt(const void* data, uint64_t size)
+uint64_t popcnt(const void* data, uint64_t size)
+{
+  uint64_t cnt = 0;
+
+  const uint8_t* data8 = (const uint8_t*) data;
+  align8(data8, size, cnt);
+
+  cnt += popcnt64_unrolled((const uint64_t*) data8, size / 8);
+  data8 += size - size % 8;
+  size = size % 8;
+
+  for (uint64_t i = 0; i < size; i++)
+    cnt += popcnt64(data8[i]);
+
+  return cnt;
+}
+
+#endif
+
+// x86 CPUs
+#if defined(__x86_64__) || \
+    defined(__i386__) || \
+    defined(_M_X64) || \
+    defined(_M_IX86)
+
+/// Count the number of 1 bits in the data array.
+/// @param data  An array
+/// @param size  Size of data in bytes
+///
+uint64_t popcnt(const void* data, uint64_t size)
 {
   static const int cpuid =
       has_POPCNT() | has_AVX2();
 
   uint64_t cnt = 0;
   const uint8_t* data8 = (const uint8_t*) data;
+
+#if defined(HAS_AVX2)
 
   // AVX2 requires arrays >= 512 bytes
   if ((cpuid & bit_AVX2) &&
@@ -554,6 +523,8 @@ static uint64_t popcnt(const void* data, uint64_t size)
     data8 += size - size % 32;
     size = size % 32;
   }
+
+#endif
 
   if (cpuid & bit_POPCNT)
   {
@@ -580,6 +551,8 @@ static uint64_t popcnt(const void* data, uint64_t size)
   return cnt;
 }
 
-#endif /* avx2 */
+#endif /* x86 */
+
+} // namespace
 
 #endif /* LIBPOPCNT_H */
