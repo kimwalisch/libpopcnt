6998ef568d78f740d64f3dfeb361b5afdb3fcf1f
Kim Walisch <kim.walisch@gmail.com>
2017-11-08 21:16:48 +0100

    Add test case for only 1 bits

--- test/test1.cpp
+++ test/test1.cpp
@@ -21,6 +21,26 @@
 #include <cstdlib>
 #include <stdint.h>
 
+void test(std::vector<uint8_t>& data, int i)
+{
+  int size = data.size();
+  double percent = (100.0 * i) / size;
+  std::cout << "\rStatus: " << (int) percent << "%" << std::flush;
+
+  uint64_t bits = popcnt(&data[i], size - i);
+  uint64_t bits_verify = 0;
+
+  for (int j = i; j < size; j++)
+    bits_verify += popcount64(data[j]);
+
+  if (bits != bits_verify)
+  {
+    std::cerr << std::endl;
+    std::cerr << "libpopcnt test failed!" << std::endl;
+    std::exit(1);
+  }
+}
+
 int main(int argc, char* argv[])
 {
   int size = 100000;
@@ -28,30 +48,19 @@ int main(int argc, char* argv[])
   if (argc > 1)
     size = std::atoi(argv[1]);
 
-  std::vector<uint8_t> data(size);
+  // init array with only 1 bits
+  std::vector<uint8_t> data(size, 0xff);
   srand((unsigned) time(0));
 
+  if (size > 0)
+    test(data, 0);
+
+  // generate array with random data
   for (int i = 0; i < size; i++)
     data[i] = (uint8_t) rand();
 
-  for (int i = 0; i < size; i++)
-  {
-    double percent = (100.0 * i) / size;
-    std::cout << "\rStatus: " << (int) percent << "%" << std::flush;
-
-    uint64_t bits = popcnt(&data[i], size - i);
-    uint64_t bits_verify = 0;
-
-    for (int j = i; j < size; j++)
-      bits_verify += popcount64(data[j]);
-
-    if (bits != bits_verify)
-    {
-      std::cerr << std::endl;
-      std::cerr << "libpopcnt test failed!" << std::endl;
-      return 1;
-    }
-  }
+  for (int i = 1; i < size; i++)
+    test(data, i);
 
   std::cout << "\rStatus: 100%" << std::endl;
   std::cout << "libpopcnt tested successfully!" << std::endl;
